e1823f84332ae8f0703985ee451da8308bee7067
phuc tran <phuctran@phucs-Mac-mini.local>
2022-05-27 10:02:36 +0700

    init

--- mobile-consumer/modules/module_widgets/lib/module_widgets.dart
+++
@@ -1,8 +0,0 @@
-library module_widgets;
-
-export 'app_image/app_provider_image.dart';
-export 'module/module.dart';
-export 'page/base_page.dart';
-export 'page/no_internet_page.dart';
-export 'page/header/member_header.dart';
-export 'page/header/provider_header.dart';
--- mobile-consumer/modules/module_widgets/lib/user_info_cubit/index.dart
+++
@@ -1,2 +0,0 @@
-export 'user_info_cubit.dart';
-export 'user_info_state.dart';
